535393a5ac6d6bb53dd89e31ea848491d3d7424b
scalarnvector <wings2248@gmail.com>
2026-04-16 12:26:28 +0900

    저장소 초기화 및 정리

---
+++ week07/practice.sh
@@ -0,0 +1,50 @@
+  182  git remote add origin https://github.com/scalarnvector/Linux-System-Programming.git
+  183  git push -u origin master
+  184  git remote remove origin
+  185  git remote add origin https://github.com/scalarnvector/Linux-System-Programming.git
+  186  ping -c 3 google.com
+  187  git push -u origin master
+  188  git config -- global credential.helper store
+  189  git config --global credential.helper store
+  190  vi test.txt
+  191  vi test2.txt
+  192  vi hw2.c
+  193  etc.passwd
+  194  etc/passwd
+  195  /etc/passwd
+  196  echo
+  197  echo linux
+  198  ls *
+  199  echo $Shell
+  200  echo $SHELL
+  201  ls *.c
+  202  mkdir backup
+  203  cp *.c backup
+  204  ls t*[0-9]
+  205  echo My host name is *
+  206  echo uname -n
+  207  echo "My host name is * 'uname -n'"
+  208  echo "My host name is *" "uname -n"
+  209  echo "My host name is *" `uname -n`
+  210  echo 'My host name is * `uname -n`'
+  211  echo My host name is * `uname -n`
+  212  ls .
+  213  vi hw2.c
+  214  vi hw6.c
+  215  gcc hw6.c -o hw6
+  216  vu hw6.c
+  217  vi hw6.c
+  218  gcc hw6.c -o hw6
+  219  gcc hw6.c -o hw6 2> compile_error
+  220  cat compile_error
+  221  ./hw6 > output
+  222  cat output
+  223  ./hw6 16 >> output
+  224  cat output
+  225  history 50 > week/practice.sh
+  226  mkdir week07
+  227  cd ~
+  228  mkdir Linux-Practices
+  229  cd Linux-Practices
+  230  mkdir week07
+  231  history 50 > week07/practice.sh
